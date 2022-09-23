e0403948e737d9d29c3faaee24d7c0ce9739f2ae
bebr <bobik33@gav.ru>
2022-09-23 16:21:04 +0500

    new

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 
-def bubble_sort(arr):
+def bubble_sort(arr, swapDirect):
     def swap(i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
@@ -13,15 +13,22 @@ def bubble_sort(arr):
         swapped = False
         x = x + 1
         for i in range(1, n - x):
-            if arr[i - 1] > arr[i]:
-                swap(i - 1, i)
-                swapped = True
+            if (swapDirect == 0):
+                if arr[i - 1] < arr[i]:
+                    swap(i - 1, i)
+                    swapped = True
+            if (swapDirect == 1):
+                if arr[i - 1] > arr[i]:
+                    swap(i - 1, i)
+                    swapped = True
+
 
 if __name__ == "__main__":
-    n = int(input())
+    direct = int(input("0 - по убыванию, 1 - по возрастанию: "))
+    n = int(input("n = "))
     arr = []
     for i in range(0, n):
         arr.append(int(random.random() * 100))
     print(arr)
-    bubble_sort(arr)
+    bubble_sort(arr, direct)
     print(arr)
